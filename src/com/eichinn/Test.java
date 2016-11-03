b6dc01862ffdb37fd5d9ade2ef69367bcee4b51a
EiChinn <eichinn77@gmail.com>
2016-11-03 22:26:31 +0800

    Initial commit

---
+++ src/com/eichinn/Test.java
@@ -0,0 +1,11 @@
+package com.eichinn;
+
+
+/**
+ * Created by chenrong on 2016/11/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
