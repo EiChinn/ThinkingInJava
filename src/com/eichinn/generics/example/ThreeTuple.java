0afa440f8438035f85124420e4dd11b4a5349425
EiChinn <eichinn77@gmail.com>
2016-11-03 22:43:55 +0800

    tuple use generics

---
+++ src/com/eichinn/generics/example/ThreeTuple.java
@@ -0,0 +1,22 @@
+package com.eichinn.generics.example;
+
+/**
+ * Created by chenrong on 2016/11/3.
+ */
+public class ThreeTuple<A, B, C> extends TwoTuple<A, B> {
+    public final C third;
+
+    public ThreeTuple(A first, B second, C third) {
+        super(first, second);
+        this.third = third;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreeTuple{" +
+                "first=" + first +
+                ", second=" + second +
+                ", third=" + third +
+                '}';
+    }
+}
