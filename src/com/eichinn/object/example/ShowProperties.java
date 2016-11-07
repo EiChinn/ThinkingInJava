0e4175998357af7231fd85311a2bbf8c89e738ca
EiChinn <eichinn77@gmail.com>
2016-11-07 20:36:42 +0800

    chapter 2

---
+++ src/com/eichinn/object/example/ShowProperties.java
@@ -0,0 +1,12 @@
+package com.eichinn.object.example;
+
+/**
+ * Created by chenrong on 2016/11/7.
+ */
+public class ShowProperties {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
