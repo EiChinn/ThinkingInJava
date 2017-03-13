a9b304ce49a907479eeb637762cf3429e803b4d6
EiChinn <eichinn77@gmail.com>
2017-03-13 21:10:00 +0800

    chapter 12:rethrow (new) exceptions

--- src/com/eichinn/exceptions/examples/InheritingExceptions.java
+++ src/com/eichinn/exceptions/examples/InheritingExceptions.java
@@ -9,17 +9,36 @@ public class InheritingExceptions {
         System.out.println("Throw SimpleException from f()");
         throw new SimpleException();
     }
+    public void g() throws SimpleException {
+        System.out.println("Throw SimpleException from g()");
+        throw new SimpleException("Originated in g()");
+    }
 
     public static void main(String[] args) {
         InheritingExceptions sed = new InheritingExceptions();
         try {
             sed.f();
         } catch (SimpleException e) {
-            System.out.println("Caught it!");
+//            System.out.println("Caught it!");
+            e.printStackTrace(System.out);//指定由System.out打印错误信息
         }
+
+        try {
+            sed.g();
+        } catch (SimpleException e) {
+//            System.out.println("Caught it!");
+            e.printStackTrace();//默认由System.err打印错误信息
+        }
+
+
     }
 }
 
 class SimpleException extends Exception {
+    public SimpleException() {
+    }
 
+    public SimpleException(String message) {
+        super(message);
+    }
 }
