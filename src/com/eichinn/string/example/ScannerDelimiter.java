2e10234e93ee251cda9f436b9d9877cd8bd924c0
EiChinn <eichinn77@gmail.com>
2017-04-29 10:38:27 +0800

    chapter 13 String : regular expression scanner delimiter

---
+++ src/com/eichinn/string/example/ScannerDelimiter.java
@@ -0,0 +1,16 @@
+package com.eichinn.string.example;
+
+import java.util.Scanner;
+
+/**
+ * Created by ei_chinn on 2017/4/29.
+ */
+public class ScannerDelimiter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("12, 42, 78, 99, 42");
+        scanner.useDelimiter("\\s*,\\s*");
+        while (scanner.hasNextInt()) {
+            System.out.println(scanner.nextInt());
+        }
+    }
+}
